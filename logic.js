56fa49ebac9b10f6f0fa21cde35fc1bc4be226bc
roy769 <roy@mortgallery.com>
2018-12-27 15:38:22 -0800

    update all

--- logic.js
+++ logic.js
@@ -4,3 +4,9 @@ $(function () {
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+$('#myModalDos').on('shown.bs.modal', function () {
+  $('#myInputDos').trigger('focus')
+})
+$('#myModalAlso').on('shown.bs.modal', function () {
+  $('#myInputAlso').trigger('focus')
+})
